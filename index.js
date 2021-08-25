26668582c57d359c332ccdec39f45a0f074b3155
Michael Newman <therealmichaelnewman@gmail.com>
2021-08-25 07:30:25 -0400

    commit tree 02626aabd71cc4336765a03551edd5fe95c39708

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData (name, email){
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+    })
+    .then(resp => resp.json())
+    .then(obj => {
+        // const body = document.querySelector('body')
+        // const newId = document.createElement('p')
+        // newId.textContent = obj.id
+        //body.append(newId)
+        document.body.textContent = obj.id
+    })
+    .catch(function (error) {
+        document.body.textContent = error.message
+      })
+
+}
+
+submitData()
